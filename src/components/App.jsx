4d099f67d2de35f1ba4b17a454cc2699d221a2c7
Yaroslav Oliinyk <4106677@gmail.com>
2022-12-16 19:36:16 +0200

    add App hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component, useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import Filter from './Filter/Filter';
@@ -8,19 +8,8 @@ import { nanoid } from 'nanoid';
 import { Report } from 'notiflix/build/notiflix-report-aio';
 
 import { Section, H1 } from './App.styled';
-import { toHaveAccessibleDescription } from '@testing-library/jest-dom/dist/matchers';
 
 export function App() {
-  // state = {
-  //   contacts: [
-  //     { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
-  //     { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
-  //     { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
-  //     { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
-  //   ],
-  //   filter: '',
-  // };
-
   const [contacts, setContacts] = useState(() => {
     return (
       JSON.parse(localStorage.getItem('contacts')) ?? [
@@ -37,20 +26,12 @@ export function App() {
     setContacts(contacts.filter(contact => contact.id !== contactId));
   };
 
-  // const deleteContact = contactId => {
-  // this.setState(({ contacts }) => ({
-  //   contacts: contacts.filter(contact => contact.id !== contactId),
-  // }));
-  // };
-
   const addContact = ({ name, number }) => {
     const contact = {
       id: nanoid(),
       name,
       number,
     };
-
-    // const { contacts } = this.state;
 
     if (
       contacts.find(
@@ -59,7 +40,7 @@ export function App() {
     ) {
       Report.info(name, ' is already in contacts.', 'Okay');
     } else {
-      this.setState({ contacts: [contact, ...contacts] });
+      setContacts([contact, ...contacts]);
     }
   };
 
@@ -76,26 +57,6 @@ export function App() {
   useEffect(() => {
     window.localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
-
-  /*
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    if (contacts) {
-      this.setState({ contacts: JSON.parse(contacts) });
-    }
-  }
-  
-
-  componentDidUpdate(_, prevState) {
-    if (toHaveAccessibleDescription.state !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-  */
-
-  // const { filter } = this.state;
-
-  // const filteredContacts = this.getFilteredContacts();
 
   return (
     <Section>
